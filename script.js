35d758b23a69937e262b2f52dda2705e8825bcab
lautylambach <lautylambach98@outlook.com.ar>
2022-05-14 23:16:31 -0300

    comentarios

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ class Lista {
     }
     
 }
-
+//variables del DOM
+//Fecha
 let fechaNumero = document.getElementById('fechaNumero')
 let fechaMes= document.getElementById('fechaMes')
 let fechaAnno= document.getElementById('fechaAnno')
 let fechaTexto= document.getElementById('fechaTexto')
-
+//Listas
 let contenedorListas = document.querySelector('[data-listas]')
 let nuevaListaForm = document.querySelector('[data-nueva-lista-form]')
 let nuevaListaInput = document.querySelector('[data-nueva-lista-input]')
@@ -19,28 +20,27 @@ let botonBorrarListas = document.querySelector('[data-borrar-lista-btn]')
 let contenedorListaMostrar = document.querySelector('[data-lista-contenedor]') 
 let tituloLista = document.querySelector('[data-titulo-lista]')
 let contadorLista = document.querySelector('[data-contador-lista]')
+//Tareas
 let tareasContenedor = document.querySelector('[data-tareas]')
 let tareaTemplate = document.getElementById('tarea-template')
 let nuevaTareaForm = document.querySelector('[data-nueva-tarea-form]')
 let nuevaTareaInput = document.querySelector('[data-nueva-tarea-input]')
 let botonBorrarTareas = document.querySelector('[data-borrar-tareas-completas]')
 
-
-
-
-
+//local storage
 let listas = JSON.parse(localStorage.getItem('listasStorages')) || []
 let idListaSelect = localStorage.getItem('SelectListaId')
 
 
-
+//event listeners
+//seleccionador de lista
 contenedorListas.addEventListener('click', e =>{
     if (e.target.tagName.toLowerCase() === 'li'){
        idListaSelect = e.target.dataset.listaId
        crearYGuardar()
     }
 })
-
+//seleccionador de tarea y check
 tareasContenedor.addEventListener('click', e => {
    if(e.target.tagName.toLowerCase() === 'input'){
        let listaSeleccionada = listas.find(lista => lista.id === idListaSelect)
@@ -51,7 +51,7 @@ tareasContenedor.addEventListener('click', e => {
 
    } 
 })
-
+// input de nombre de lista
 nuevaListaForm.addEventListener('submit', e => {
    e.preventDefault() 
    nombreLista = nuevaListaInput.value
@@ -61,6 +61,7 @@ nuevaListaForm.addEventListener('submit', e => {
    listas.push(lista)
    crearYGuardar()
 })
+//input nombre tarea
 nuevaTareaForm.addEventListener('submit', e => {
     e.preventDefault() 
     nombreTarea = nuevaTareaInput.value
@@ -71,19 +72,22 @@ nuevaTareaForm.addEventListener('submit', e => {
     nuevaTareaInput.value = null
     crearYGuardar()
  })
-
+// boton borrar tareas ompletas
 botonBorrarTareas.addEventListener('click', e => {
     let listaSeleccionada = listas.find(lista =>lista.id === idListaSelect)
     listaSeleccionada.tareas = listaSeleccionada.tareas.filter(tarea => !tarea.completada)
     crearYGuardar()
 })
-
+//boton borrar listas
 botonBorrarListas.addEventListener('click', e =>{
     listas = listas.filter(lista => lista.id !== idListaSelect )
     idListaSelect = null
     crearYGuardar()
 })
 
+//funciones
+
+//funcion que genera la fecha actual
 let crearFecha = () => {
     let fecha = new Date()
     fechaNumero.textContent = fecha.toLocaleString('es', {day:'numeric'})
@@ -92,51 +96,34 @@ let crearFecha = () => {
     fechaAnno.textContent = fecha.toLocaleString('es', {year:'numeric'})
 
 }
-
+//funcion crear tarea
 function crearTarea(nombre){
     return {id:Date.now().toString(), nombre: nombre, completada: false}
 }
-
+//funcion crear lista
 function crearLista(nombre){
    return let = new Lista (Date.now().toString(), nombre,[])
 }
-
+//guardar local storage
 function guardar(){
     localStorage.setItem('listasStorages',JSON.stringify(listas))
     localStorage.setItem('SelectListaId',JSON.stringify(idListaSelect))
 }
-
+// funcion crear+guardar
 function crearYGuardar(){
    guardar() 
    crear()
    
 }
 
-function crear(){
-    limpiarElementos(contenedorListas)
-    crearListas()
-    crearFecha()
-    let listaSeleccionada = listas.find(lista => lista.id === idListaSelect) ?? console.log('seleccione lista ') 
-    
-        if(idListaSelect == null){
-            contenedorListaMostrar.style.display = 'none'
-        }else{
-            try{
-            contenedorListaMostrar.style.display =''
-            tituloLista.innerText = listaSeleccionada.nombre //error si no se selecciona una lista -- se debera aplicar un try finally
-            crearContador(listaSeleccionada)
-            limpiarElementos(tareasContenedor)
-            crearTareas(listaSeleccionada)
-            }catch{
-                tituloLista.innerText = 'Seleccione una lista'
-            }
-            
-        }
-        
-    
-    
+// funcion que crea el visor de tareas sin realizar
+function crearContador(listaSeleccionada){
+    let tareasIncompletasContador = listaSeleccionada.tareas.filter(tarea => !tarea.completada).length
+    let tareaString = tareasIncompletasContador === 1 ? "tarea" : "tareas"
+    contadorLista.innerText = `${tareasIncompletasContador} ${tareaString} sin completar `
 }
 
+//funcion que crea el contenido para las tareas
 function crearTareas(listaSeleccionada){
     listaSeleccionada.tareas.forEach(tarea =>{
         let elementoTarea =document.importNode(tareaTemplate.content, true)
@@ -150,12 +137,7 @@ function crearTareas(listaSeleccionada){
     })
 }
 
-function crearContador(listaSeleccionada){
-    let tareasIncompletasContador = listaSeleccionada.tareas.filter(tarea => !tarea.completada).length
-    let tareaString = tareasIncompletasContador === 1 ? "tarea" : "tareas"
-    contadorLista.innerText = `${tareasIncompletasContador} ${tareaString} sin completar `
-}
-
+// funcion que crea el contenido de listas
 function crearListas(){
     listas.forEach(lista =>{
         let elementoLista = document.createElement('li')
@@ -169,10 +151,33 @@ function crearListas(){
     })
 }
 
+// funcion que limpia los elementos previos del html
 function limpiarElementos(element){
     while(element.firstChild)
     element.removeChild(element.firstChild)
 }
 
+//funcion que crea la estructura
+function crear(){
+    limpiarElementos(contenedorListas)
+    crearListas()
+    crearFecha()
+    let listaSeleccionada = listas.find(lista => lista.id === idListaSelect) ?? console.log('seleccione lista ') 
+    
+    if(idListaSelect == null){
+        contenedorListaMostrar.style.display = 'none'
+    }else{
+        try{
+            contenedorListaMostrar.style.display =''
+            tituloLista.innerText = listaSeleccionada.nombre //error si no se selecciona una lista -- se debera aplicar un try finally
+            crearContador(listaSeleccionada)
+            limpiarElementos(tareasContenedor)
+            crearTareas(listaSeleccionada)
+        }catch{
+            tituloLista.innerText = 'Seleccione una lista'
+            } 
+        }
+}
 
+//se llama la funcion inicial
 crear()
